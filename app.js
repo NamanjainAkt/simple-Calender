3f059c18af07d4374d114a85c9b7b026d8624680
NamanjainAkt <162738854+NamanjainAkt@users.noreply.github.com>
2025-01-20 12:41:01 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+const date = document.getElementById("date");
+const day = document.getElementById("day");
+const month = document.getElementById("month");
+const tear = document.getElementById("year");
+const today = new Date();
+const weekdays = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+const allMonths = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+date.innerHTML =  (today.getDate()<10?"0":"") + today.getDate();
+day.innerHTML = weekdays[today.getDay()]
+month.innerHTML = allMonths[today.getMonth()]
+year.innerHTML = today.getFullYear();
